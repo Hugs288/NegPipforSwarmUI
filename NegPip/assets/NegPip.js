469fb866e102f8e2fa33a76c8d5cc403bee0cea6
Hugs288 <131965855+Hugs288@users.noreply.github.com>
2025-04-26 20:17:47 +0330

    Delete NegPip directory

--- NegPip/assets/NegPip.js
+++
@@ -1,6 +0,0 @@
-postParamBuildSteps.push(() => {
-    let dynThreshGroup = document.getElementById('input_group_content_negpip');
-    if (dynThreshGroup && !currentBackendFeatureSet.includes('negpip')) {
-        dynThreshGroup.append(createDiv(`negpip_install_button`, 'keep_group_visible', `<button class="basic-button" onclick="installFeatureById('negpip', 'negpip_install_button')">Install NegPip</button>`));
-    }
-});
