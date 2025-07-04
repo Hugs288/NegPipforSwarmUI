86ec1c4e52818480467e687c7009cb2c0c5dbf4d
Hugs288 <benyamin.mardani321@gmail.com>
2025-07-04 19:28:12 +0330

    actual  install button

--- assets/NegPip.js
+++ assets/NegPip.js
@@ -1,6 +1,1 @@
-postParamBuildSteps.push(() => {
-    let NegPipGroup = document.getElementById('input_group_content_sampling');
-    if (NegPipGroup && !currentBackendFeatureSet.includes('negpip')) {
-        NegPipGroup.append(createDiv(`negpip_install_button`, 'keep_group_visible', `<button class="basic-button" onclick="installFeatureById('negpip', 'negpip_install_button')">Install NegPip</button>`));
-    }
-});
+addInstallButton('sampling', 'negpip', 'negpip', 'Install NegPip');
