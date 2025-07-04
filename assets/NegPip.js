a6426945adac9073f52bc69c9cd9bd8a575239b1
Hugs288 <benyamin.mardani321@gmail.com>
2025-07-04 19:11:34 +0330

    revert install button

--- assets/NegPip.js
+++ assets/NegPip.js
@@ -1,19 +1,6 @@
 postParamBuildSteps.push(() => {
-    setInterval(() => {
-        let NegPipParamGroup = document.getElementById('input_group_content_sampling');
-        if (!NegPipParamGroup) {
-            return; // Exit if the parent element isn't on the page
-        }
-        let installButton = document.getElementById('negpip_install_button');
-        let isInstalled = currentBackendFeatureSet.includes('negpip');
-
-        if (isInstalled && installButton) {
-            // If it's installed and the button exists, remove the button.
-            installButton.remove();
-        }
-        else if (!isInstalled && !installButton) {
-            // If it's not installed and the button doesn't exist, create it.
-            NegPipParamGroup.append(createDiv('negpip_install_button', 'keep_group_visible', `<button class="basic-button" onclick="installFeatureById('negpip', 'negpip_install_button')">Install NegPip</button>`));
-        }
-    }, 1000);
+    let NegPipGroup = document.getElementById('input_group_content_sampling');
+    if (NegPipGroup && !currentBackendFeatureSet.includes('negpip')) {
+        NegPipGroup.append(createDiv(`negpip_install_button`, 'keep_group_visible', `<button class="basic-button" onclick="installFeatureById('negpip', 'negpip_install_button')">Install NegPip</button>`));
+    }
 });
